e0e35fe36d41d804e9b7a857fd6fb8fbc7afa327
Spencer Summerville <me@grenuttag.com>
2020-09-01 14:26:25 -0400

    Rewrite Caption as a function block

--- src/components/Caption.js
+++ src/components/Caption.js
@@ -1,25 +1,27 @@
-const Caption = ({
-  id,
-  startInSeconds,
-  endInSeconds,
-  text,
-  updateCaption,
-  isActive,
-}) => (
-  <div class={isActive ? "caption caption-highlighted" : "caption"}>
-    <div class="timestamps">
-      <input type="text" class="startTimestamp" value={startInSeconds} />
-      <input type="text" class="endTimeStamp" value={endInSeconds} />
+export default function Caption(props) {
+  let {
+    id,
+    startInSeconds,
+    endInSeconds,
+    text,
+    updateCaption,
+    isActive,
+  } = props
+
+  return (
+    <div class={isActive ? "caption caption-highlighted" : "caption"}>
+      <div class="timestamps">
+        <input type="text" class="startTimestamp" value={startInSeconds} />
+        <input type="text" class="endTimeStamp" value={endInSeconds} />
+      </div>
+
+      <textarea
+        class="caption-textbox"
+        name="editable_text"
+        value={text}
+        rows="3"
+        onInput={event => updateCaption(id, event.target.value)}
+      />
     </div>
-
-    <textarea
-      class="caption-textbox"
-      name="editable_text"
-      value={text}
-      rows="3"
-      onInput={event => updateCaption(id, event.target.value)}
-    />
-  </div>
-)
-
-export default Caption
+  )
+}
